3697c5ac148980ee1d91df353b52ea7a10a1be79
Molly McKenzie <160646541+mollymckenzie@users.noreply.github.com>
2024-09-18 13:08:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,93 +1,106 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
-
-// Import each image properly so Webpack processes them correctly
-import birdpic1 from "./images/NorthernParula.PNG";
-import birdpic2 from "./images/pw.PNG";
-import birdpic3 from "./images/AmericanRobin.PNG";
-import birdpic4 from "./images/BlueJay.PNG";
-import birdpic5 from "./images/NorthernCardinal.PNG";
-import birdpic6 from "./images/sp.PNG";
-import birdpic7 from "./images/a.PNG";
-import birdpic8 from "./images/RedWingedBlackbird.PNG";
-import birdpic9 from "./images/EasternPhoebe.PNG";
-import birdpic10 from "./images/SongSparrow.PNG";
-import birdpic11 from "./images/IndigoBunting.PNG";
-import birdpic12 from "./images/YellowRumpedWarbler.PNG";
-import birdpic13 from "./images/BarnSwallow.PNG";
-import birdpic14 from "./images/CedarWaxwing.PNG";
-import birdpic15 from "./images/EasternKingbird.PNG";
-import birdpic16 from "./images/DarkEyedJunco.PNG";
-import birdpic17 from "./images/NorthernMockingbird.PNG";
-import birdpic18 from "https://imgur.com/a/Wqj0QUB";
-import birdpic19 from "./images/TennesseeWarbler.PNG";
-import birdpic20 from "./images/SummerTanger.PNG";
-
-const rootElement = document.getElementById("root");
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+// Image data
 const buttonData = [
-  { id: 1, imageSrc: birdpic1, text: "Northern Parula" },
-  { id: 2, imageSrc: birdpic2, text: "Prothonotary Warbler" },
-  { id: 3, imageSrc: birdpic3, text: "American Robin" },
-  { id: 4, imageSrc: birdpic4, text: "Blue Jay" },
-  { id: 5, imageSrc: birdpic5, text: "Northern Cardinal" },
-  { id: 6, imageSrc: birdpic6, text: "Savannah Sparrow" },
-  { id: 7, imageSrc: birdpic7, text: "American Goldfinch" },
-  { id: 8, imageSrc: birdpic8, text: "Red-winged Blackbird" },
-  { id: 9, imageSrc: birdpic9, text: "Eastern Phoebe" },
-  { id: 10, imageSrc: birdpic10, text: "Song Sparrow" },
-  { id: 11, imageSrc: birdpic11, text: "Indigo Bunting" },
-  { id: 12, imageSrc: birdpic12, text: "Yellow Rumped Warbler" },
-  { id: 13, imageSrc: birdpic13, text: "Barn Swallow" },
-  { id: 14, imageSrc: birdpic14, text: "Cedar Waxwing" },
-  { id: 15, imageSrc: birdpic15, text: "Eastern Kingbird" },
-  { id: 16, imageSrc: birdpic16, text: "Dark-eyed Junco" },
-  { id: 17, imageSrc: birdpic17, text: "Northern Mockingbird" },
-  { id: 18, imageSrc: "https://imgur.com/a/Wqj0QUB", text: "Eastern Bluebird" },
-  { id: 19, imageSrc: birdpic19, text: "Tennessee Warbler" },
-  { id: 20, imageSrc: birdpic20, text: "Summer Tanger" },
+  { id: 1, imageSrc: "./images/NorthernParula.PNG", text: "Northern Parula" },
+  { id: 2, imageSrc: "./images/pw.PNG", text: "Prothonotary Warbler" },
+  { id: 3, imageSrc: "./images/AmericanRobin.PNG", text: "American Robin" },
+  { id: 4, imageSrc: "./images/BlueJay.PNG", text: "Blue Jay" },
+  {
+    id: 5,
+    imageSrc: "./images/NorthernCardinal.PNG",
+    text: "Northern Cardinal",
+  },
+  { id: 6, imageSrc: "./images/sp.PNG", text: "Savannah Sparrow" },
+  { id: 7, imageSrc: "./images/a.PNG", text: "American Goldfinch" },
+  {
+    id: 8,
+    imageSrc: "./images/RedWingedBlackbird.PNG",
+    text: "Red-winged Blackbird",
+  },
+  { id: 9, imageSrc: "./images/EasternPhoebe.PNG", text: "Eastern Phoebe" },
+  { id: 10, imageSrc: "./images/SongSparrow.PNG", text: "Song Sparrow" },
+  { id: 11, imageSrc: "./images/IndigoBunting.PNG", text: "Indigo Bunting" },
+  {
+    id: 12,
+    imageSrc: "./images/YellowRumpedWarbler.PNG",
+    text: "Yellow Rumped Warbler",
+  },
+  { id: 13, imageSrc: "./images/BarnSwallow.PNG", text: "Barn Swallow" },
+  { id: 14, imageSrc: "./images/CedarWaxwing.PNG", text: "Cedar Waxwing" },
+  {
+    id: 15,
+    imageSrc: "./images/EasternKingbird.PNG",
+    text: "Eastern Kingbird",
+  },
+  { id: 16, imageSrc: "./images/DarkEyedJunco.PNG", text: "Dark-eyed Junco" },
+  {
+    id: 17,
+    imageSrc: "./images/NorthernMockingbird.PNG",
+    text: "Northern Mockingbird",
+  },
+  {
+    id: 18,
+    imageSrc: "./images/EasternBluebird.PNG",
+    text: "Eastern Bluebird",
+  },
+  {
+    id: 19,
+    imageSrc: "./images/TennesseeWarbler.PNG",
+    text: "Tennessee Warbler",
+  },
+  { id: 20, imageSrc: "./images/SummerTanger.PNG", text: "Summer Tanager" },
 ];
 
-export function MyButtonGrid() {
-  const [visibleButtonId, setVisibleButtonId] = useState(null);
-  const [wigglingButtonId, setWigglingButtonId] = useState(null);
+const buttonGrid = document.querySelector(".button-grid");
+let visibleButtonId = null;
+let wigglingButtonId = null;
 
-  const handleClick = (id) => {
-    setVisibleButtonId(visibleButtonId === id ? null : id);
-    setWigglingButtonId(id);
+// Function to create buttons dynamically
+buttonData.forEach(({ id, imageSrc, text }) => {
+  const buttonContainer = document.createElement("div");
 
-    setTimeout(() => setWigglingButtonId(null), 500);
-  };
+  // Create the image button
+  const button = document.createElement("button");
+  button.classList.add("image-button");
+  button.style.backgroundImage = `url(${imageSrc})`;
 
-  return (
-    <div className="button-grid">
-      {buttonData.map(({ id, imageSrc, text }) => (
-        <div key={id}>
-          <button
-            className={`image-button ${
-              wigglingButtonId === id ? "wiggle" : ""
-            }`}
-            onClick={() => handleClick(id)}
-            style={{ backgroundImage: `url(${imageSrc})` }}
-          ></button>
+  // Handle button click
+  button.addEventListener("click", () => handleClick(id));
 
-          <div
-            className={`pulldown-text ${visibleButtonId === id ? "show" : ""}`}
-          >
-            <p>{text}</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  // Create the text container
+  const textContainer = document.createElement("div");
+  textContainer.classList.add("pulldown-text");
+  textContainer.innerHTML = `<p>${text}</p>`;
+
+  buttonContainer.appendChild(button);
+  buttonContainer.appendChild(textContainer);
+  buttonGrid.appendChild(buttonContainer);
+});
+
+function handleClick(id) {
+  const currentText = document.querySelectorAll(".pulldown-text");
+
+  currentText.forEach((element, index) => {
+    // Toggle the visibility of the text for the clicked button
+    if (buttonData[index].id === id) {
+      element.classList.toggle("show");
+    } else {
+      element.classList.remove("show");
+    }
+  });
+
+  const buttons = document.querySelectorAll(".image-button");
+
+  // Add wiggle effect to the clicked button
+  buttons.forEach((btn, index) => {
+    if (buttonData[index].id === id) {
+      btn.classList.add("wiggle");
+    } else {
+      btn.classList.remove("wiggle");
+    }
+  });
+
+  // Remove wiggle effect after 500ms
+  setTimeout(() => {
+    buttons.forEach((btn) => btn.classList.remove("wiggle"));
+  }, 500);
 }
-
